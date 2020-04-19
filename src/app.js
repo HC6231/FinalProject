13a7276ec135d561029b00b8931c2c524c26148e
HaoranChen <haoranchentiger@gmail.com>
2020-04-19 19:10:56 -0400

    update

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+require('dotenv').config();
 //db setup 
 require('./db')
 
