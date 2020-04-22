a701eddc05ffa6eb99d0ef9ac5a40fe1fd2e8953
HaoranChen <haoranchentiger@gmail.com>
2020-04-22 03:48:35 -0400

    update

--- src/app.js
+++ src/app.js
@@ -2,25 +2,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const publicPath = path.resolve(__dirname, '../');
+const publicPath = path.resolve(__dirname,'../');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
-//DB START HERE
+//db setup 
 require('./db')
 
-//Passport Strategy Set Up
+//passport config
 require('./passport_config')(passport);
 
-//Import Models
+//import model from DB
+const mongoose = require('mongoose');
 const loginInfo = mongoose.model('userLogin');
 const userDiary = mongoose.model('Diary');
-
-//avoid method confusion
 mongoose.set('useFindAndModify', false);
-
 //bycrypt setup
 const bcrypt = require('bcrypt');
 
@@ -35,60 +32,56 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(flash());
 
-
-//route handler
-app.get('/', function (req, res) {
+app.get('/',function(req,res){
     res.render('login');
 })
 
-app.post('/', function (req, res, next) {
-    passport.authenticate('local', {
-        successRedirect: '/main',
-        failureRedirect: '/',
-        failureFlash: true
-    })
-        (req, res, next);
+app.post('/',function (req, res, next) {
+    passport.authenticate('local',{
+        successRedirect:'/main',
+        failureRedirect:'/',
+        failureFlash: true})
+        (req,res,next);
 });
 
 
-app.get('/register', function (req, res) {
+app.get('/register',function(req,res){
     res.render('register');
 });
 
 
-app.post('/register', function (req, res) {
-    checkExist(req.body.userName, result => {
-        if (result) {
-            req.flash('info', 'User Existed');
-            res.render('register', { message: req.flash('info') });
+app.post('/register',function(req,res){
+    checkExist(req.body.userName, result=>{
+        if(result){
+            req.flash('info','User Existed');
+            res.render('register',{message: req.flash('info')});           
         }
-        if (!result) {
-            if (req.body.userName == '' || req.body.userPsd == '') {
-                req.flash('info', 'Something is missing');
-                res.render('register', { message: req.flash('info') });
+        if(!result){
+            if(req.body.userName == '' || req.body.userPsd ==''){
+                req.flash('info','Something is missing');
+                res.render('register',{message: req.flash('info')});
             }
-            else if (req.body.userPsd != req.body.confirmPsd) {
-                req.flash('info', "Password doesn't match, please confirm.")
-                res.render('register', { message: req.flash('info') });
+            else if(req.body.userPsd != req.body.confirmPsd){
+                req.flash('info',"Password doesn't match, please confirm.")
+                res.render('register',{message: req.flash('info')});
             }
-            else {
-                const hashedPassword = bcrypt.hashSync(req.body.userPsd, 10);
-                saveRegInfo(req.body.userName, hashedPassword);
+            else{
+                const hashedPassword = bcrypt.hashSync(req.body.userPsd,10);
+                saveRegInfo(req.body.userName,hashedPassword);
                 res.redirect('/main');
             }
         }
     })
 });
 
-
-app.get('/main', checkAuthenticated, function (req, res) {
+app.get('/main',checkAuthenticated,function(req,res){
     res.render('main');
 });
 
-
-app.post('/main', async function (req, res) {
+app.post('/main',async function(req,res){
     const today = getToday();
     console.log(today);
     const userInput = JSON.stringify(req.body);
@@ -97,30 +90,29 @@ app.post('/main', async function (req, res) {
     const user = req.user;
     //push saved diary into user information via objID
     const savedDiary = await saveDiary(inputObj.date, inputObj.subject, inputObj.context, user._id);
-    const updatedUser = await loginInfo.findOneAndUpdate({ _id: savedDiary.user }, { $push: { diary: savedDiary._id } });
-    req.flash('info', 'User Existed');
+    const updatedUser = await loginInfo.findOneAndUpdate({_id: savedDiary.user},  {$push: { diary: savedDiary._id}});
+    req.flash('info','User Existed');
     res.redirect('/main');
 });
 
 
-app.get('/record', checkAuthenticated, async function (req, res) {
-    const userInfo = await loginInfo.find();
+app.get('/record', checkAuthenticated ,async function(req,res){
+    const userInfo =  await loginInfo.find();
     console.log(userInfo)
     const record = userInfo[0].diary;
-    const allRecord = await userDiary.find({ _id: { $in: record } });
-    console.log(typeof (allRecord));
-    console.log({ allRecord });
-    res.render('record', { allRecord });
+    const allRecord =  await userDiary.find({_id:{$in:record}});
+    console.log(typeof(allRecord));
+    console.log({allRecord});
+    res.render('record',{allRecord});
 })
 
 
-app.get('/logout', function (req, res) {
+app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
 });
 
-//helper function
-function getToday() {
+function getToday(){
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -129,32 +121,29 @@ function getToday() {
     return today;
 }
 
-
-function checkExist(query, cb) {
-    loginInfo.findOne({ email: query }, function (err, user) {
-        if (user !== null) {
+function checkExist(query, cb){
+    loginInfo.findOne({email: query}, function(err,user){
+        if(user !== null){
             cb(true);
         }
-        else {
+        else{
             cb(false);
         }
     })
 }
 
-
-async function saveRegInfo(email, password) {
+async function saveRegInfo(email, password){
     const insert = new loginInfo({
         email: email,
         password: password
     })
-    insert.save(function (err) {
-        if (err) (console.log(err));
+    insert.save(function(err){
+        if(err)(console.log(err));
     })
 }
 
-
-async function saveDiary(date, subject, context, user) {
-    try {
+async function saveDiary(date, subject, context, user){
+    try{
         const insertDiary = new userDiary({
             date: date,
             subject: subject,
@@ -163,26 +152,22 @@ async function saveDiary(date, subject, context, user) {
         })
         const saved = await insertDiary.save();
         return saved;
-    } catch (err) {
+    }catch(err){
         console.log(err);
         res.status(500).send(err)
     }
 }
 
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
         return next();
     }
     res.redirect('/');
 }
 
-//START THE SERVER
+
 let port = process.env.PORT;
-if (port == null || port == "") {
+if(port == null || port == ""){
     port = 3000;
 }
 app.listen(port)
-
-
-//git push heroku master
